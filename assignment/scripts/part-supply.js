a26e1af2b6b530f00c3706d06671a1eb14873545
Crystal Redmon <cato2848@gmail.com>
2022-08-29 15:11:07 -0500

    completed remaining questions including stretch goals

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -52,9 +52,26 @@ console.log('---  Stretch Goals  ---');
 // 7. Rewrite the `for` loop from #6 as a `for of` loop. 
 console.log('7. Showing supplyChanges with "for of" loop');
 
+for( const x of supplyChanges){
+    if(x>0){
+        console.log(`Added ${x} parts`);
+    }else if(x===0){
+        console.log('No Change.');
+    }else{
+        console.log(`Removed ${x*-1} parts.`)
+    }
+}
+
+
 // 8. Write a loop to determine the total number of parts available by
 //    adding up all the numbers in the 'supplyChanges' array.
-console.log('8. Total supplies available is:');
+let totalSupplies = 0 
+for(let i=0; i<supplyChanges.length; i++){
+    totalSupplies += supplyChanges[i];
+}
+
+console.log('8. Total supplies available is:', totalSupplies);
+
 
 // 9. We have a large stash of parts in our warehouse that we 
 //    need to box up and get ready for shipment. 
@@ -62,4 +79,15 @@ console.log('8. Total supplies available is:');
 //    Use a `while` loop to keep adding parts to boxes until
 //    no more boxes can be filled.
 //    Then log how many boxes were filled, and how many parts are left over.
+
+
 console.log('9. Filling boxes with a "while" loop');
+let boxes = 0;
+let unboxedParts = 572;
+
+while(unboxedParts > 7){ 
+    boxes++;
+    unboxedParts -= 7;
+}
+
+console.log(`We were able to fill ${boxes} boxes. We still have ${unboxedParts} parts left over.`)
